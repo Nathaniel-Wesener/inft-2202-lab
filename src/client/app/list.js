af72b0e8549a09b4273883de0b89be8407950d50
Nathaniel Wesener <nathaniel.wesener@dcmail.ca>
2024-06-04 20:51:19 -0400

    added pagination to the list along with delete confirmation and having the cards in a nice row

--- src/client/app/list.js
+++ src/client/app/list.js
@@ -5,7 +5,9 @@ if (products.length === 0) {
     const eleDisclaimer = document.getElementById('noProduct');
     eleDisclaimer.classList.remove('d-none');
 }
-else{
+else if (products.length > 4){
+    drawProductCardsPagination(products);
+} else {
     drawProductCards(products);
 }
 
@@ -79,10 +81,25 @@ function addCard(product, div) {
     newButtonTrashImg.height = "20";
     newDelete.appendChild(newButtonTrashImg);
     newDelete.addEventListener('click', (event) =>{
-        const deleted = deleteProduct(product.name);
-        if (deleted) {
-            window.location.reload();
-        }
+        const eleModal = document.getElementById('confirm-modal');
+        eleModal.style.display = "block";
+        const eleModalClose = document.getElementById('modal-close');
+        eleModalClose.addEventListener('click', (event) =>{
+            eleModal.style.display = "none";
+        });
+        const eleModalX = document.getElementById('modal-x');
+        eleModalX.addEventListener('click', (event) =>{
+            eleModal.style.display = "none";
+        });
+        const eleModalDelete = document.getElementById('modal-delete');
+        eleModalDelete.addEventListener('click', (event) =>{
+            const deleted = deleteProduct(product.name);
+    
+            if (deleted) {
+                window.location.reload();
+            }
+        });
+
     });
     newDiv.appendChild(newDelete);
 
@@ -90,4 +107,101 @@ function addCard(product, div) {
     div.appendChild(newCard);
     const space = document.createElement("br");
     div.appendChild(space);
+}
+
+function drawProductCardsPagination(list) {
+    
+    const remainder = list.length % 4;
+    let numPages;
+
+    if (remainder === 0) {
+        numPages = list.length / 4;    
+    } else {
+        numPages = ((list.length - remainder ) / 4 ) + 1;
+    }
+    
+    
+    let currentPage = 1;
+    const eleMaster = document.getElementById('pagination-master')
+    eleMaster.classList.remove('d-none');
+
+    const eleNav = document.getElementById('page-links');
+    
+    let newItem = document.createElement("li");
+    newItem.classList.add('page-item');
+    let newLink = document.createElement("a");
+    newLink.classList.add('page-link');
+    newLink.textContent = "Prev";
+    newLink.href = "#";
+    newLink.addEventListener('click', (event) => {
+        currentPage  = setPage(list, currentPage - 1, numPages);
+    });
+    newItem.appendChild(newLink);
+    eleNav.appendChild(newItem);
+    
+    for (let index = 0; index < numPages ; index++) {
+        newItem = document.createElement("li");
+        newItem.classList.add('page-item');
+        newLink = document.createElement("a");
+        newLink.classList.add('page-link');
+        newLink.textContent = `${ index + 1 }`;
+        newLink.href = "#";
+        newLink.addEventListener('click', (event) => {
+            currentPage  = setPage(list, index + 1, numPages);
+        });
+        newItem.appendChild(newLink);
+        eleNav.appendChild(newItem);  
+    }
+
+    newItem = document.createElement("li");
+    newItem.classList.add('page-item');
+    newLink = document.createElement("a");
+    newLink.classList.add('page-link');
+    newLink.textContent = "Next";
+    newLink.href = "#";
+    newLink.addEventListener('click', (event) => {
+        currentPage  = setPage(list, currentPage + 1, numPages);
+    });
+    newItem.appendChild(newLink);
+    eleNav.appendChild(newItem);
+
+    setPage(list, currentPage, numPages);
+}
+
+function setPage(list, page, numPages) {
+    
+    const eleNav = document.getElementById('page-links');
+
+    for (let iterator of eleNav.children) {
+        iterator.classList.remove('active');
+    }
+    
+    if ( page === 1 ) {
+        eleNav.children[0].classList.add('disabled');
+        
+    } else {
+        eleNav.children[0].classList.remove('disabled');
+    }
+
+    if ( page === numPages ) {
+        eleNav.children[numPages + 1].classList.add('disabled');
+    } else {
+        eleNav.children[numPages + 1].classList.remove('disabled');
+    }
+
+    eleNav.children[page].classList.add('active');
+
+    const div = document.getElementById('product-display');
+    div.innerHTML = "";
+    
+    for (let index = 0; index < 4; index++) {
+        
+        if (( (page - 1 ) * 4 )  + index > list.length - 1) {
+            break;
+        }
+        addCard(list[ ( (page - 1 ) * 4 )  + index], div);
+    }
+        
+    
+    return page;
 }
